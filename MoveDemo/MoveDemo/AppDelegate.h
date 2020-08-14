c8f89bdb2213d769b96b7c376585a2f2a6c6b6eb
新增 <ly-yangliu@MacBook-Pro.local>
2020-08-14 15:18:54 +0800

    测试

---
+++ MoveDemo/MoveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoveDemo
+//
+//  Created by lulu  on 16/10/18.
+//  Copyright © 2016年 lulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
